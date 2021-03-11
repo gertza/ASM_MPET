e1b08e84c77eeb8bd7962bc40ef3fe03bee653e6
unknown <C:\Users\Max\AppData\Roaming\The Bat!\Mail>
2021-03-11 10:31:57 +0300

    Добавьте файлы проекта.

---
+++ ASM_MPET.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int result = 0;
+	int in_x, in_y = 0;
+	int x, y = 0;
+	cin >> x;
+	cin >> y;
+	_asm {
+		// код на ассемблере
+		XOR EAX, EAX
+		XOR EBX, EBX
+		MOV EAX, 5
+		MOV EBX, x
+		ADD EAX, EBX
+		PUSH EAX
+		XOR EAX, EAX
+		POP EAX
+		MOV result, EAX
+		XOR EAX, EAX
+		XOR EBX, EBX
+		MOV EAX, x
+		MOV EBX, y
+		PUSH EAX
+		PUSH EBX
+		POP EAX
+		POP EBX
+		MOV in_x, EBX
+		MOV in_y, EAX
+	}
+	cout << result << endl;
+	cout << "In x " << in_x << endl;
+	cout << "In y " << in_y << endl;
+}
